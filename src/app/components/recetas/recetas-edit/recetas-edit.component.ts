e104b5f594cb2b90719c9cfbedf6cc505f8f3f87
emeery <gerardoemery@live.com.mx>
2018-12-07 17:23:09 -0600

    edita receta

--- src/app/components/recetas/recetas-edit/recetas-edit.component.ts
+++ src/app/components/recetas/recetas-edit/recetas-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { RecetaService } from '../recetas-lista/recetas.service';
 @Component({
   selector: 'app-recetas-edit',
@@ -11,32 +11,58 @@ export class RecetasEditComponent implements OnInit {
   id: number;
   recetaForma: FormGroup;
   modoEditar = false;
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private route: ActivatedRoute,
     private recetaServicio: RecetaService) {}
-
   ngOnInit() {
     this.route.params
       .subscribe((params: Params) => {
         this.id = +params['id'];
-        this.modoEditar = params['id'] !== null;
+        this.modoEditar = params['id'] != null;
         this.initForm();
       });
+  }
+  onSubmit() {
+   console.log(this.recetaForma);
+  }
+  onAddIngrediente() {
+    (<FormArray>this.recetaForma.get('ingredientes')).push(
+      new FormGroup({
+        'nombre': new FormControl(),
+        'monto': new FormControl()
+      })
+    );
   }
   private initForm() {
     let recetaNombre = '';
     let recetaImagenRuta = '';
     let recetaDescripcion = '';
+    const recetaIngredientes = new FormArray([]);
     if (this.modoEditar) {
       const receta = this.recetaServicio.getReceta(this.id);
-      console.log(receta);
+      // console.log(receta);
       recetaNombre = receta.nombre;
       recetaImagenRuta = receta.imagenRuta;
       recetaDescripcion = receta.descripcion;
+      if (receta['ingredientes']) {
+        for (const ing of receta.ingredientes) {
+          recetaIngredientes.push(
+            new FormGroup({
+              'nombre': new FormControl(ing.nombre),
+              'monto': new FormControl(ing.monto)
+            })
+          );
+        }
+      }
     }
     this.recetaForma = new FormGroup({
-      'nombre' : new FormControl(recetaNombre),
-      'imagenRuta' : new FormControl(recetaImagenRuta),
-      'descripcion' : new FormControl(recetaDescripcion),
+      'nombre': new FormControl(recetaNombre),
+      'imagenRuta': new FormControl(recetaImagenRuta),
+      'descripcion': new FormControl(recetaDescripcion),
+      'ingredientes': recetaIngredientes
     });
+  }
+  getControls() {
+  return (<FormArray>this.recetaForma.get('ingredientes')).controls;
   }
 }
--- src/app/components/recetas/recetas-lista/recetas.service.ts
+++ src/app/components/recetas/recetas-lista/recetas.service.ts
@@ -1,5 +1,5 @@
 import { Receta } from '../model/receta.model';
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingrediente
 } from '../../compatido/model/ingrediente.model';
 import { ComprasListaService
@@ -16,9 +16,9 @@ export class RecetaService {
         new Ingrediente('Lonchas de Queso Cheddar', 4)
       ] ),
       new Receta(
-        'Alitas BBQ',
-        '1/2 Kg de Alitas con salsa especial BBQ',
-        'https://image.flaticon.com/icons/png/512/168/168554.png',
+        'Ensalada César',
+        '1/2 Kg de Vegetales con aderezo especial',
+        'https://image.flaticon.com/icons/png/512/168/168522.png',
         [
           new Ingrediente('Pan Especial', 2),
           new Ingrediente('Lonchas de Queso Cheddar', 4)
